a09301ae36b342c9a5e9113278b9e914fa5d725a
Stefan507 <stefan.bos1@outlook.com>
2023-12-05 11:43:50 +0100

    Initialize files

---
+++ README.md
@@ -0,0 +1,8 @@
+ # How the game works
+ In this game, the user can guess a number between 1 and 100. 
+ The user can guess 4 times, each time the score will decrease
+ There are 5 rounds, in wich the user can score points, from 0 to 3
+ If the guess is correct in the first guess, the user will get 3 points
+ On the second attempt this will be 2, then 1 and finally 0
+ There will be hints. The hints are:
+ Higher, Loower, multiples, divisable 
---
+++ main.py
@@ -0,0 +1,31 @@
+
+import random
+
+def main():
+    random_number = generate_random_number()
+    print(random_number)
+
+#TODO: Generate random number between 1 and 100
+def generate_random_number():
+    """
+    Generates a random number between 1 and 100.
+
+    Returns:
+        int: A random number between 1 and 100.
+    """
+    random_number = random.randint(1, 100)
+    return random_number
+
+#TODO: create 5 games 
+
+#TODO: create rounds in the game, 4 rounds
+#TODO: get input from the user
+
+#TODO: get input from the user
+
+#TODO: Make a point system
+
+#TODO: Output the score the user got
+
+if __name__ == "__main__":
+    main()
