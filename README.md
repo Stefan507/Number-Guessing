76438d1f2a680972ce967177336015da9dfde7aa
Stefan507 <stefan.bos1@outlook.com>
2023-12-05 14:53:11 +0100

    Day 1

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
  # How the game works
- In this game, the user can guess a number between 1 and 100. 
+ In this game, the user can guess a number between 1 and 10. 
  The user can guess 4 times, each time the score will decrease
  There are 5 rounds, in wich the user can score points, from 0 to 3
  If the guess is correct in the first guess, the user will get 3 points
- On the second attempt this will be 2, then 1 and finally 0
+ On the second attempt this will be 2, then 1 and finally 0.5
  There will be hints. The hints are:
- Higher, Loower, multiples, divisable 
+ Smaller, larger
+
+ ## Workflow
+ When the program is run:
+ - Initialize games counter, set counter to 0
+ - Initialize round to 0
+    - Generate random integer
+    - Let the user guess the number
+        - If correct -> 3 points
+        - If incorrect -> Give hint
+        - If correct -> 2 points
+        - If incorrect -> Give hint
+        - If correct -> 1 points
+        - If incorrect -> Give hint
+        - If correct -> 0.5 points
+        - If incorrect -> Show answer + award 0 points
+    - Increment the games counter with 1
+- Repeat above step 4 more times
+- Ouput the score of the user
+
+    
--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 import random
 
 def main():
-    random_number = generate_random_number()
-    print(random_number)
+    games = 0
+    round = 0
+    while games < 5:    
+        random_number = generate_random_number()
+        while round < 4:
+            answer = input("Guess: ")
+            points = validate_answer(answer, random_number, round)
+            round += 1
+        games += 1
+    print(float(points))
 
-#TODO: Generate random number between 1 and 100
+
 def generate_random_number():
     """
     Generates a random number between 1 and 100.
@@ -13,19 +21,42 @@ def generate_random_number():
     Returns:
         int: A random number between 1 and 100.
     """
-    random_number = random.randint(1, 100)
+    random_number = random.randint(1, 10)
     return random_number
 
-#TODO: create 5 games 
+def validate_answer(answer, random_number, round):
+    points = 0
+    if round == 0:
+        if int(answer) == random_number:
+            points += 3
+        elif int(answer) > random_number:
+            print("Smaller")
+        elif int(answer) < random_number:
+            print("Larger")
+    if round == 1:
+        if int(answer) == random_number:
+            points += 2
+        elif int(answer) > random_number:
+            print("Smaller")
+        elif int(answer) < random_number:
+            print("Larger")
+    if round == 2:
+        if int(answer) == random_number:
+            points += 1
+        elif int(answer) > random_number:
+            print("Smaller")
+        elif int(answer) < random_number:
+            print("Larger")
+    if round == 3:
+        if int(answer) == random_number:
+            points += 0.5
+        elif int(answer) > random_number:
+            print(f"The answer was: {random_number}")
+        
+    return float(points)
 
-#TODO: create rounds in the game, 4 rounds
-#TODO: get input from the user
 
-#TODO: get input from the user
-
-#TODO: Make a point system
-
-#TODO: Output the score the user got
 
 if __name__ == "__main__":
     main()
+     
