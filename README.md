ddd2105639ac14fb7d03d08fc5d49c062e28b8ce
Stefan507 <stefan.bos1@outlook.com>
2023-12-06 15:06:49 +0100

    Finished

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
- # How the game works
+ # Number Guessing
+ Welcome to the wonderfull game of guessing the number. How many points can you score?
+ 
+ ## How the game works
  In this game, the user can guess a number between 1 and 10. 
- The user can guess 4 times, each time the score will decrease
- There are 5 rounds, in wich the user can score points, from 0 to 3
- If the guess is correct in the first guess, the user will get 3 points
- On the second attempt this will be 2, then 1 and finally 0.5
+ The user can guess 4 times.
+ There are 5 rounds, in wich the user can score points, 1 for each correct answer.
  There will be hints. The hints are:
  Smaller, larger
 
@@ -11,15 +12,18 @@
  When the program is run:
  - Initialize games counter, set counter to 0
  - Initialize round to 0
+ - Set the score to 0
+ - Welcome the user to the game
+ - Print in which game the user currently is
     - Generate random integer
     - Let the user guess the number
-        - If correct -> 3 points
+        - If correct -> 1 point
         - If incorrect -> Give hint
-        - If correct -> 2 points
+        - If correct -> 1 point
         - If incorrect -> Give hint
-        - If correct -> 1 points
+        - If correct -> 1 point
         - If incorrect -> Give hint
-        - If correct -> 0.5 points
+        - If correct -> 1 point
         - If incorrect -> Show answer + award 0 points
     - Increment the games counter with 1
 - Repeat above step 4 more times
--- main.py
+++ main.py
@@ -1,62 +1,63 @@
-
 import random
 
 def main():
-    games = 0
-    round = 0
-    while games < 5:    
-        random_number = generate_random_number()
-        while round < 4:
-            answer = input("Guess: ")
-            points = validate_answer(answer, random_number, round)
-            round += 1
-        games += 1
-    print(float(points))
-
-
-def generate_random_number():
     """
-    Generates a random number between 1 and 100.
+    The `main` function is the entry point of the program. 
+    It prompts the user to guess a number between 1 and 10 for a total of 5 games. 
+    It calls the `generate_number` function to generate a random number for each game and passes it to the `play_game` function. 
+    The `play_game` function allows the user to make up to 4 guesses and keeps track of the number of correct guesses. 
+    Finally, the `main` function prints the total number of correct guesses made by the user.
+    
+    Inputs: None
+    Outputs: The total number of correct guesses made by the user.
+    """
+    print("Welcome the number guessing, can you guess the number between 1 and 10?")
+    score = 0
+    for games in range(5):
+        print(f"This is game {games + 1}")
+        random_number = generate_number()
+        score += play_game(random_number)
+    print(f"You guessed {score} numbers correct!")
+
+def play_game(random_number):
+    """
+    Play a number guessing game.
+
+    Args:
+    random_number (int): The randomly generated number that the user needs to guess.
 
     Returns:
-        int: A random number between 1 and 100.
+    int: The number of correct guesses made by the user.
+    """
+    score = 0
+    for round in range(4):
+        while True:
+            answer = input("Guess: ")
+            if answer.isdigit() and 1 <= int(answer) <= 10:
+                break
+            else:
+                print("Invalid input. Please enter an integer between 1 and 10.")
+        if int(answer) == random_number:
+            score += 1
+            print("Correct!")
+            break
+        elif int(answer) < random_number:
+            print("Larger")
+        elif int(answer) > random_number:
+            print("Smaller")
+    else:
+        print(f"Incorrect, the number was {random_number}")
+    return score
+
+def generate_number():
+    """
+    Generates a random number between 1 and 10.
+
+    Returns:
+        int: A random number between 1 and 10.
     """
     random_number = random.randint(1, 10)
     return random_number
 
-def validate_answer(answer, random_number, round):
-    points = 0
-    if round == 0:
-        if int(answer) == random_number:
-            points += 3
-        elif int(answer) > random_number:
-            print("Smaller")
-        elif int(answer) < random_number:
-            print("Larger")
-    elif round == 1:
-        if int(answer) == random_number:
-            points += 2
-        elif int(answer) > random_number:
-            print("Smaller")
-        elif int(answer) < random_number:
-            print("Larger")
-    elif round == 2:
-        if int(answer) == random_number:
-            points += 1
-        elif int(answer) > random_number:
-            print("Smaller")
-        elif int(answer) < random_number:
-            print("Larger")
-    elif round == 3:
-        if int(answer) == random_number:
-            points += 0.5
-        elif int(answer) > random_number or int(answer) < random_number:
-            print(f"The answer was: {random_number}")
-        
-    return float(points)
-
-
-
 if __name__ == "__main__":
     main()
-     
